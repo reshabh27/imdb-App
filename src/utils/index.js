39c40adea88659f82eb4f5c8b2301e9ada73f9fc
Reshabh <reshabhsahu10@gmail.com>
2024-01-23 13:03:51 +0530

    Added render link for api

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const productionUrl = " http://localhost:3000";
+const productionUrl = "https://imdb-api-26h7.onrender.com/";
 
 export const customFetch = axios.create({
   baseURL: productionUrl,
